2aacab0098f5ee367727f07f20cd63e4c2d0a191
sawarerohini <132651092+sawarerohini@users.noreply.github.com>
2024-07-22 14:52:45 +0530

    Update test_first_webpagatomat.py

--- src/test_first_webpagatomat.py
+++ src/test_first_webpagatomat.py
@@ -9,7 +9,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 def test_abc():
     # Initialize Firefox WebDriver
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 
     driver.get("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C589460569891%7Cb%7Cfacebook%20signin%7C&placement=&creative=589460569891&keyword=facebook%20signin&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696221832%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-3821998899%26loc_physical_ms%3D9040229%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAjwpuajBhBpEiwA_ZtfhYOUN3f61gMqn-EWjxqcm0EEcR2GmAWBTRGX-bDq7k4oaBlGhrzzIRoCjlUQAvD_BwE")
